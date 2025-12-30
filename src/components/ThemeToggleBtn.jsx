aae744c22352f2b6c75d48c8fde4d5dd80e6b8df
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2025-12-30 12:54:53 +0530

    navbar is completed

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import assets from '../assets/assets'
 
-const ThemeToggleBtn = () => {
+const ThemeToggleBtn = ({theme,setTheme}) => {
+
+    useEffect(()=>{
+        const prefersDarkMode = window.matchMedia('(prefers-color-schema: dark)').matches
+
+        setTheme(theme || (prefersDarkMode ? 'dark' : 'light'))
+
+    },[])
+
+    useEffect(()=>{
+
+        if(theme === 'dark'){
+            document.documentElement.classList.add('dark')
+        }else{
+            document.documentElement.classList.remove('dark')
+        }
+
+        localStorage.setItem('theme',theme)
+
+    },[theme])
   return (
     <>
-    <button>{theme==='dark'? ():()}</button>
+    <button>{theme==='dark'? (<img onClick = {()=>setTheme('light')} src={assets.sun_icon} className='size-8.5 p-1.5 border border-gray-500 rounded-full'/>):(<img onClick = {()=>setTheme('dark')} src={assets.moon_icon} className='size-8.5 p-1.5 border border-gray-500 rounded-full'/>)}</button>
     </>
   )
 }
