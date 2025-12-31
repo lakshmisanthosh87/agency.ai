066fc7b23009271dc1120de3e10b5526e39b977d
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2025-12-31 13:11:43 +0530

    footer section is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
+import Footer from './components/Footer'
 
 const App = () => {
   // const [theme , setTheme] = useState("light")
@@ -17,6 +19,8 @@ const App = () => {
       <Services/>
       <OurWork/>
       <Teams/>
+      <ContactUs/>
+      <Footer theme={theme} setTheme={setTheme} />
     </div>
   )
 }
